0f4243bb3f36e97f9484612cde72b7f91df0dbb8
BrunoSilvaSaba <brunossaba.tech@gmail.com>
2021-01-12 20:58:37 -0300

    V - 12.012021

--- js/main.js
+++ js/main.js
@@ -2,16 +2,18 @@
     var header_area = document.getElementById('headers');
 
     function headerEnter_highlighted_color() {
-        header_area.style.backgroundColor = "rgb(113, 35, 35)";
-        header_area.style.boxShadow = " 1px 1px 3px darkgray"
+        header_area.style.backgroundColor = "white";
+        header_area.style.boxShadow = "none"
     }
     function headerOut() {
         header_area.style.backgroundColor = "white"
-        header_area.style.boxShadow = "none"
+        
+        header_area.style.boxShadow = "2px 2px 10px black"
     }
     
     function hightColorColor() {
         var Color_color = document.getElementsByTagName('td');
 
         Color_color.style.Color = 'red'
+        
     }
